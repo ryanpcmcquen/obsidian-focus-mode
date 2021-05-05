d31c01c8ed942ca0e75f5e94e1fdeba5f0361d85
Ryan McQuen <rpcm@linux.com>
2021-05-05 10:15:14 -0700

    Focus mode works!
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -1,112 +1,42 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		console.log('loading plugin');
+		console.log('Loading Focus Mode plugin ...');
 
-		await this.loadSettings();
+		this.addRibbonIcon('enter', 'Toggle Focus Mode', (event): void => {
+			const app_container =
+				// @ts-ignore
+				this.app.dom.appContainerEl ||
+				document.querySelector('.app-container');
+			const view_content = app_container.querySelector('.view-content');
+			const collapse_buttons = app_container.querySelectorAll(
+				'.workspace-ribbon-collapse-btn'
+			);
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+			Array.from(collapse_buttons).forEach((collapse_button) => {
+				const is_not_collapsed =
+					// @ts-ignore
+					collapse_button.getAttribute('aria-label') !== 'Expand';
 
-		this.addStatusBarItem().setText('Status Bar Text');
-
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
+				if (is_not_collapsed) {
+					// @ts-ignore
+					collapse_button.click();
 				}
-				return false;
+			});
+
+			if (app_container.style.visibility !== 'hidden') {
+				app_container.style.visibility = 'hidden';
+			} else {
+				app_container.style.visibility = 'visible';
 			}
+			view_content.style.visibility = 'visible';
+			// @ts-ignore
+			event.target.style.visibility = 'visible';
 		});
-
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log('Unloading Focus Mode plugin ...');
 	}
 }
