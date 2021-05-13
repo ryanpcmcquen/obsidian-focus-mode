3714c615f2275dcc8d1a84fa412a110a9ba86cca
Ryan McQuen <RyanMcQuen@stockx.com>
2021-05-13 11:29:55 -0700

    Close splits using workspace methods, rather than dom clicking.
    Signed-off-by: Ryan McQuen <RyanMcQuen@stockx.com>

--- main.ts
+++ main.ts
@@ -1,35 +1,25 @@
-import { Plugin } from 'obsidian';
+import { Plugin } from "obsidian";
 
 export default class FocusMode extends Plugin {
 	async onload() {
-		console.log('Loading Focus Mode plugin ...');
+		console.log("Loading Focus Mode plugin ...");
 
-		this.addRibbonIcon('enter', 'Toggle Focus Mode', (event): void => {
+		this.addRibbonIcon("enter", "Toggle Focus Mode", (event): void => {
 			const app_container =
 				// @ts-ignore
 				this.app.dom.appContainerEl ||
-				document.querySelector('.app-container');
+				document.querySelector(".app-container");
 
-			const collapse_buttons = app_container.querySelectorAll(
-				'.workspace-ribbon-collapse-btn'
-			);
+			app_container.classList.toggle("focus_mode");
 
-			Array.from(collapse_buttons).forEach((collapse_button) => {
-				const is_not_collapsed =
-					// @ts-ignore
-					collapse_button.getAttribute('aria-label') !== 'Expand';
-
-				if (is_not_collapsed) {
-					// @ts-ignore
-					collapse_button.click();
-				}
-			});
-
-			app_container.classList.toggle('focus_mode');
+			// @ts-ignore
+			this.app.workspace.leftSplit.collapse();
+			// @ts-ignore
+			this.app.workspace.rightSplit.collapse();
 		});
 	}
 
 	onunload() {
-		console.log('Unloading Focus Mode plugin ...');
+		console.log("Unloading Focus Mode plugin ...");
 	}
 }
