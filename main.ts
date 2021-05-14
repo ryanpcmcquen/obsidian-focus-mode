21220b661fb28068bd027629a982f451805d85ab
Ryan McQuen <rpcm@linux.com>
2021-05-14 09:12:43 -0700

    Convert main file to spaces.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
 import { Plugin } from "obsidian";
 
 export default class FocusMode extends Plugin {
-	async onload() {
-		console.log("Loading Focus Mode plugin ...");
+    async onload() {
+        console.log("Loading Focus Mode plugin ...");
 
-		this.addRibbonIcon("enter", "Toggle Focus Mode", (event): void => {
-			const app_container =
-				// @ts-ignore
-				this.app.dom.appContainerEl ||
-				document.querySelector(".app-container");
+        this.addRibbonIcon("enter", "Toggle Focus Mode", (event): void => {
+            const app_container =
+                // @ts-ignore
+                this.app.dom.appContainerEl ||
+                document.querySelector(".app-container");
 
-			app_container.classList.toggle("focus-mode");
+            app_container.classList.toggle("focus-mode");
 
-			// @ts-ignore
-			this.app.workspace.leftSplit.collapse();
-			// @ts-ignore
-			this.app.workspace.rightSplit.collapse();
-		});
-	}
+            // @ts-ignore
+            this.app.workspace.leftSplit.collapse();
+            // @ts-ignore
+            this.app.workspace.rightSplit.collapse();
+        });
+    }
 
-	onunload() {
-		console.log("Unloading Focus Mode plugin ...");
-	}
+    onunload() {
+        console.log("Unloading Focus Mode plugin ...");
+    }
 }
