da281ae02f62e67ba05e17e5bde63336e233d9b1
Ryan McQuen <rpcm@linux.com>
2021-05-16 14:11:58 -0700

    Add Super Focus mode.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -4,19 +4,45 @@ export default class FocusMode extends Plugin {
     async onload() {
         console.log("Loading Focus Mode plugin ...");
 
-        this.addRibbonIcon("enter", "Toggle Focus Mode", (event): void => {
-            const app_container =
+        this.addRibbonIcon(
+            "enter",
+            "Toggle Focus Mode (Shift + Click to show active pane only)",
+            (event): void => {
+                if (
+                    event.shiftKey ||
+                    // @ts-ignore
+                    this.app.workspace.rootSplit.containerEl.hasClass(
+                        "maximised"
+                    )
+                ) {
+                    // @ts-ignore
+                    this.app.workspace.rootSplit.containerEl.toggleClass(
+                        "maximised",
+                        // @ts-ignore
+                        !this.app.workspace.rootSplit.containerEl.hasClass(
+                            "maximised"
+                        )
+                    );
+                    // @ts-ignore
+                    this.app.workspace.onLayoutChange();
+                }
+
+                const app_container =
+                    // @ts-ignore
+                    this.app.dom.appContainerEl ||
+                    document.querySelector(".app-container");
+
+                app_container.classList.toggle(
+                    "focus-mode",
+                    !app_container.classList.contains("focus-mode")
+                );
+
                 // @ts-ignore
-                this.app.dom.appContainerEl ||
-                document.querySelector(".app-container");
-
-            app_container.classList.toggle("focus-mode");
-
-            // @ts-ignore
-            this.app.workspace.leftSplit.collapse();
-            // @ts-ignore
-            this.app.workspace.rightSplit.collapse();
-        });
+                this.app.workspace.leftSplit.collapse();
+                // @ts-ignore
+                this.app.workspace.rightSplit.collapse();
+            }
+        );
     }
 
     onunload() {
