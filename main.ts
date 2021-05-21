316875969bcd7c3d2302f7881bcf8d3d88d4f2f0
Ryan McQuen <rpcm@linux.com>
2021-05-21 08:26:15 -0700

    Add hotkeys and command palette stuff for Focus Modes.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -1,6 +1,37 @@
 import { Plugin } from "obsidian";
 
 export default class FocusMode extends Plugin {
+    toggleFocusMode(superFocus?: boolean) {
+        if (
+            superFocus ||
+            // @ts-ignore
+            this.app.workspace.rootSplit.containerEl.hasClass("maximised")
+        ) {
+            // @ts-ignore
+            this.app.workspace.rootSplit.containerEl.toggleClass(
+                "maximised",
+                // @ts-ignore
+                !this.app.workspace.rootSplit.containerEl.hasClass("maximised")
+            );
+            // @ts-ignore
+            this.app.workspace.onLayoutChange();
+        }
+
+        if (superFocus && !document.body.classList.contains("focus-mode")) {
+            document.body.classList.add("focus-mode");
+        } else {
+            document.body.classList.toggle(
+                "focus-mode",
+                !document.body.classList.contains("focus-mode")
+            );
+        }
+
+        // @ts-ignore
+        this.app.workspace.leftSplit.collapse();
+        // @ts-ignore
+        this.app.workspace.rightSplit.collapse();
+    }
+
     async onload() {
         console.log("Loading Focus Mode plugin ...");
 
@@ -8,36 +39,27 @@ export default class FocusMode extends Plugin {
             "enter",
             "Toggle Focus Mode (Shift + Click to show active pane only)",
             (event): void => {
-                if (
-                    event.shiftKey ||
-                    // @ts-ignore
-                    this.app.workspace.rootSplit.containerEl.hasClass(
-                        "maximised"
-                    )
-                ) {
-                    // @ts-ignore
-                    this.app.workspace.rootSplit.containerEl.toggleClass(
-                        "maximised",
-                        // @ts-ignore
-                        !this.app.workspace.rootSplit.containerEl.hasClass(
-                            "maximised"
-                        )
-                    );
-                    // @ts-ignore
-                    this.app.workspace.onLayoutChange();
-                }
-
-                document.body.classList.toggle(
-                    "focus-mode",
-                    !document.body.classList.contains("focus-mode")
-                );
-        
-                // @ts-ignore
-                this.app.workspace.leftSplit.collapse();
-                // @ts-ignore
-                this.app.workspace.rightSplit.collapse();
+                this.toggleFocusMode(event.shiftKey);
             }
         );
+
+        this.addCommand({
+            id: "toggle-focus-mode",
+            name: "Toggle Focus Mode",
+            callback: () => {
+                this.toggleFocusMode();
+            },
+            hotkeys: [{ modifiers: ["Alt", "Mod"], key: "F" }],
+        });
+
+        this.addCommand({
+            id: "toggle-super-focus-mode",
+            name: "Toggle Super Focus Mode (Active pane only)",
+            callback: () => {
+                this.toggleFocusMode(true);
+            },
+            hotkeys: [{ modifiers: ["Alt", "Mod", "Shift"], key: "F" }],
+        });
     }
 
     onunload() {
